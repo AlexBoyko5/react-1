8981cebd1c4e467a3aefc38c3c6175b13e82ce8e
AlexBoyko5 <144361285+AlexBoyko5@users.noreply.github.com>
2024-03-15 10:37:20 +0100

    commit-3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -16,27 +16,26 @@
 //!========= Будь-який компонент, в тому числі Product, оголошує один параметр - props,
 //! це завжди буде об'єкт, що містить усі передані пропси.
 // src/components/App.jsx
+// import Product from './Product'; // Заменяем import { Product } from './Product';
 
-import { Product } from './Product';
+// export default function App() {
+// 	return (
+// 		<div>
+// 			<h1>Best selling</h1>
 
-export default function App() {
-	return (
-		<div>
-			<h1>Best selling</h1>
-
-			<Product
-				name="Tacos With Lime"
-				imgUrl="https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640"
-				price={10.99}
-			/>
-			<Product
-				name="Fries and Burger"
-				imgUrl="https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?dpr=2&h=480&w=640"
-				price={14.29}
-			/>
-		</div>
-	);
-}
+// 			<Product
+// 				name="Tacos With Lime"
+// 				imgUrl="https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640"
+// 				price={10.99}
+// 			/>
+// 			<Product
+// 				name="Fries and Burger"
+// 				imgUrl="https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?dpr=2&h=480&w=640"
+// 				price={14.29}
+// 			/>
+// 		</div>
+// 	);
+// }
 //!========= Визначення значення за замовчуванням гарантує, що props.imgUrl матиме значення,
 //!========= навіть якщо його не вказано під час виклику компонента у батька.
 // src/components/App.jsx
@@ -55,3 +54,31 @@ export default function App() {
 // 		</div>
 // 	);
 // }
+
+//!=========//!=========//!=========//!=========//!=========//!=========//!=========
+
+const favouriteBooks = [
+	{ id: 'id-1', name: 'JS for beginners' },
+	{ id: 'id-2', name: 'React basics' },
+	{ id: 'id-3', name: 'React Router overview' },
+];
+
+const BookList = ({ books }) => {
+	return (
+		<ul>
+			{books.map((book) => {
+				return <li key={book.id}>{book.name}</li>;
+			})}
+		</ul>
+	);
+};
+
+const App = () => {
+	return (
+		<>
+			<h1>Books of the week</h1>
+			<BookList books={favouriteBooks} />
+		</>
+	);
+};
+export default App;
--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -71,19 +71,20 @@
 //!==========//!==========//!==========//!==========//!==========//!==========//!==========//!==========//!==========
 // src/components/Product.jsx
 
-// export const Product = (props) => {
-// 	return (
-// 		<div>
-// 			<h2>{props.name}</h2>
-// 			<img
-// 				src="https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640"
-// 				alt="Tacos With Lime"
-// 				width="480"
-// 			/>
-// 			<p>Price: 999 credits</p>{' '}
-// 		</div>
-// 	);
-// };
+const Product = (props) => {
+	return (
+		<div>
+			<h2>{props.name}</h2>
+			<img
+				src="https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640"
+				alt="Tacos With Lime"
+				width="480"
+			/>
+			<p>Price: 999 credits</p>{' '}
+		</div>
+	);
+};
+export default Product;
 //!======= В самому компоненті Product використаємо отримані пропси в розмітці.
 // src/components/Product.jsx
 
@@ -101,21 +102,21 @@
 //! Це робить код чистішим і читабельнішим.
 // src/components/Product.jsx
 
-export const Product = ({ name, imgUrl, price }) => {
-	return (
-		<div>
-			<h2>{name}</h2>
-			<img src={imgUrl} alt={name} width="480" />
-			<p>Price: {price} credits</p>
-		</div>
-	);
-};
+// export const Product = ({ name, imgUrl, price }) => {
+// 	return (
+// 		<div>
+// 			<h2>{name}</h2>
+// 			<img src={imgUrl} alt={name} width="480" />
+// 			<p>Price: {price} credits</p>
+// 		</div>
+// 	);
+// };
 
 //!======= Для того щоб вказати значення властивостей за замовчуванням,
 //!======= використовується синтаксис значень за замовчуванням під час деструктуризації пропсів.
 // src/components/Product.jsx
 
-// const Product = ({
+// export const Product = ({
 // 	name,
 // 	imgUrl = 'https://dummyimage.com/640x480/2a2a2a/ffffff&text=Product+image+placeholder',
 // 	price,
@@ -126,3 +127,84 @@ export const Product = ({ name, imgUrl, price }) => {
 // 		<p>Price: {price} credits</p>
 // 	</div>
 // );
+//!======= Умовний рендеринг
+
+// export const Mailbox = ({ name, unreadMessages }) => {
+// 	return (
+// 		<div>
+// 			<p>Hello {name}</p>
+// 			<p>
+// 				{unreadMessages.length > 0
+// 					? `You have ${unreadMessages.length} unread messages`
+// 					: 'No unread messages'}
+// 			</p>
+// 		</div>
+// 	);
+// };
+
+// const Mailbox = ({ username, messages }) => {
+// 	return (
+// 		<div>
+// 			<p>Hello {username}</p>
+// 			{messages.length > 0 ? (
+// 				<div>
+// 					<p>You have {messages.length} unread messages</p>
+// 					<MessageList messages={messages} />
+// 				</div>
+// 			) : (
+// 				<p>No unread messages</p>
+// 			)}
+// 		</div>
+// 	);
+// };
+
+//!=======
+//  const favouriteBooks = [
+// 	{ id: 'id-1', name: 'JS for beginners' },
+// 	{ id: 'id-2', name: 'React basics' },
+// 	{ id: 'id-3', name: 'React Router overview' },
+// ];
+
+// const BookList = ({ books }) => {
+// 	return (
+// 		<ul>
+// 			{books.map((book) => {
+// 				return <li>{book.name}</li>;
+// 			})}
+// 		</ul>
+// 	);
+// };
+
+// const App = () => {
+// 	return (
+// 		<>
+// 			<h1>Books of the week</h1>
+// 			<BookList books={favouriteBooks} />
+// 		</>
+// 	);
+// };
+//!===== Типізація пропсів (не работает) ============================================================
+// src/components/Product.jsx
+
+// import PropTypes from 'prop-types';
+
+// const Product = ({
+// 	name,
+// 	imgUrl = 'https://dummyimage.com/640x480/2a2a2a/ffffff&text=Product+image+placeholder',
+// 	price,
+// }) => (
+// 	<div>
+// 		<img src={imgUrl} alt={name} width="480" />
+// 		<h2>{name}</h2>
+// 		<p>Price: {price} credits</p>{' '}
+// 	</div>
+// );
+
+// // Опис типів пропсів компонента
+// Product.propTypes = {
+// 	name: PropTypes.string.isRequired,
+// 	imgUrl: PropTypes.string,
+// 	price: PropTypes.number.isRequired,
+// };
+
+// export default Product;
