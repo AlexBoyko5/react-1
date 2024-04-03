5a0dcc52f997bc611457e7fe7fe3cc4bef23ba61
AlexBoyko5 <144361285+AlexBoyko5@users.noreply.github.com>
2024-04-03 19:44:27 +0200

    commit-HW-3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -57,28 +57,69 @@
 
 //!=========//!=========//!=========//!=========//!=========//!=========//!=========
 
-const favouriteBooks = [
-	{ id: 'id-1', name: 'JS for beginners' },
-	{ id: 'id-2', name: 'React basics' },
-	{ id: 'id-3', name: 'React Router overview' },
-];
+// const favouriteBooks = [
+// 	{ id: 'id-1', name: 'JS for beginners' },
+// 	{ id: 'id-2', name: 'React basics' },
+// 	{ id: 'id-3', name: 'React Router overview' },
+// ];
 
-const BookList = ({ books }) => {
-	return (
-		<ul>
-			{books.map((book) => {
-				return <li key={book.id}>{book.name}</li>;
-			})}
-		</ul>
-	);
-};
+// const BookList = ({ books }) => {
+// 	return (
+// 		<ul>
+// 			{books.map((book) => {
+// 				return <li key={book.id}>{book.name}</li>;
+// 			})}
+// 		</ul>
+// 	);
+// };
 
+// const App = () => {
+// 	return (
+// 		<>
+// 			<h1>Books of the week</h1>
+// 			<BookList books={favouriteBooks} />
+// 		</>
+// 	);
+// };
+// export default App;
+//!========= ДО Д/З № 3 === //!=========//!=========//!=========//!=========//!=========//!=========
+// Импортируем необходимые библиотеки и стили
+import React, { useState } from 'react';
+import './App.css';
+
+// Компонент Contact для отображения отдельного контакта
+const Contact = ({ contact }) => (
+	<div>
+		{contact.name}: {contact.number}
+	</div>
+);
+
+// Компонент ContactList для отображения списка контактов
+const ContactList = ({ contacts }) => (
+	<div>
+		{contacts.map((contact) => (
+			<Contact key={contact.id} contact={contact} />
+		))}
+	</div>
+);
+
+// Компонент App - корневой компонент приложения
 const App = () => {
+	// Используем хук состояния для хранения контактов
+	const [contacts, setContacts] = useState([
+		{ id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+		{ id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+		{ id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+		{ id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+	]);
+
 	return (
-		<>
-			<h1>Books of the week</h1>
-			<BookList books={favouriteBooks} />
-		</>
+		<div>
+			<h1>Phonebook</h1>
+			{/* Здесь будут компоненты ContactForm и SearchBox */}
+			<ContactList contacts={contacts} />
+		</div>
 	);
 };
+
 export default App;
